ea45d2553df6d9cb7b4a5611fd9daa45a4bc6061
Hyokoji <kameyumaru@yahoo.co.jp>
2019-05-30 17:34:32 +0900

    first commit

---
+++ getline.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+
+int main()
+{
+  std::ifstream ifs("./test_matrix.csv");
+  std::string str;
+  int iaa = 0;
+  while(std::getline(ifs,str)) {
+    std::string tmp;
+    std::istringstream iss(str);
+    while(std::getline(iss,str,',')) {
+      std::cout << str << std::endl;
+    }
+    iaa++;
+  }
+
+  return 0;
+}
